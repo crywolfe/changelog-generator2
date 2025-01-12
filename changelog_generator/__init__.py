4eebf9ba0cc71364b6fbf42552d97a0ca8691b45
Gerry Wolfe <crywolfe@users.noreply.github.com>
2025-01-12 13:50:21 -0600

    feat: Add generate_ai_changelog and _list_ollama_models imports

--- changelog_generator/__init__.py
+++ changelog_generator/__init__.py
@@ -1,4 +1,5 @@
-from changelog_generator import main
+from changelog_generator import main, generate_ai_changelog
+from changelog_generator import _list_ollama_models
 
 def run_main():
     return main()
